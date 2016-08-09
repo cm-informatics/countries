01a145bf409b9d14183be5135de7293707c057a5
Christian Mansch <epost@cm-informatics.de>
2016-08-09 16:48:09 +0200

    Initial Commit

---
+++ Countries/CountriesTableViewController.swift
@@ -0,0 +1,203 @@
+//
+//  CountriesTableViewController.swift
+//  Countries
+//
+//  Created by Christian Mansch on 17.07.16.
+//  Copyright © 2016 Christian Mansch. All rights reserved.
+//
+
+import UIKit
+
+/*extension CountriesTableViewController: UISearchResultsUpdating
+{
+    func updateSearchResultsForSearchController(searchController: UISearchController)
+    {
+        //filterContentForSearchText(searchController.searchBar.text!)
+    }
+ 
+}
+*/
+
+class CountriesTableViewController: UITableViewController, UISearchBarDelegate, UISearchResultsUpdating {
+
+    var countries: NSArray = []
+    //var countries = [String]()
+    //var filteredCountries = [String]()
+    var filteredCountries: NSArray = []
+    var shouldShowSearchResults = false
+    
+    var searchController: UISearchController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureSearchController()
+        getCountries()
+    }
+    override func viewWillAppear(animated: Bool) {
+        searchController.searchBar.hidden = false
+    }
+    
+    func getCountries() {
+        
+        let JSONFile = NSBundle.mainBundle().pathForResource("countries", ofType: "json")
+        let data = NSData(contentsOfFile: JSONFile!)!
+    
+        do {
+            let json = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+             countries =  NSArray(array: json as! [AnyObject])
+            
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+        }
+        
+    }
+    
+    func configureSearchController()
+    {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search here..."
+        searchController.searchBar.delegate = self
+        searchController.searchBar.sizeToFit()
+        //definesPresentationContext = true
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.tableView.tableHeaderView = searchController.searchBar
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        
+        if searchBar.text != "" {  // To still display the search results, not the whole list
+            shouldShowSearchResults = true
+        }
+        else{
+            shouldShowSearchResults = false
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+        
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let searchString = searchController.searchBar.text
+        
+        // Filter the data array and get only those countries that match the search text.
+        filteredCountries = countries.filter({ (country) -> Bool in
+            let countryText: NSString = country.objectForKey("cn_short_en") as! NSString
+            
+            return (countryText.rangeOfString(searchString!, options: NSStringCompareOptions.CaseInsensitiveSearch).location) != NSNotFound
+        })
+        
+        // Reload the tableview.
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if shouldShowSearchResults{
+            return filteredCountries.count
+        }
+        else{
+            return countries.count
+        }
+        
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        if shouldShowSearchResults{
+            if filteredCountries[indexPath.row].valueForKey("states")!.count != 0{
+                cell.accessoryType = .DisclosureIndicator
+            }
+            else{
+                cell.accessoryType = .None
+            }
+        
+            cell.textLabel?.text = String(filteredCountries[indexPath.row].valueForKey("cn_short_en")!)
+            
+        }
+        else{
+            if countries[indexPath.row].valueForKey("states")!.count != 0{
+                cell.accessoryType = .DisclosureIndicator
+            }
+            else{
+                cell.accessoryType = .None
+            }
+            
+            cell.textLabel?.text = String(countries[indexPath.row].valueForKey("cn_short_en")!)
+        }
+        return cell
+    }
+
+    // MARK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if tableView.cellForRowAtIndexPath(indexPath)?.accessoryType == .DisclosureIndicator
+        {
+            searchController.searchBar.hidden = true
+            searchController.searchBar.resignFirstResponder()
+            
+            if !shouldShowSearchResults
+            {
+                if let states = countries[indexPath.row].valueForKey("states")
+                {
+                    let nameOftheStates = states.valueForKey("zn_name_local")
+        
+                    let stvc: StatesTableViewController = StatesTableViewController()
+                    stvc.initWithStates(nameOftheStates as! NSArray, numberOfStates: (countries[indexPath.row].valueForKey("states")?.count)!)
+        
+                    self.navigationController?.pushViewController(stvc, animated: true)
+                }
+            }
+            else
+            {
+                if let states = filteredCountries[indexPath.row].valueForKey("states")
+                {
+                    let nameOftheStates = states.valueForKey("zn_name_local")
+                    
+                    let stvc: StatesTableViewController = StatesTableViewController()
+                    stvc.initWithStates(nameOftheStates as! NSArray, numberOfStates: (filteredCountries[indexPath.row].valueForKey("states")?.count)!)
+                    
+                    self.navigationController?.pushViewController(stvc, animated: true)
+                }
+
+            }
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Countries/StatesTableViewController.swift
@@ -0,0 +1,85 @@
+//
+//  StatesTableViewController.swift
+//  Countries
+//
+//  Created by Christian Mansch on 05.08.16.
+//  Copyright © 2016 Christian Mansch. All rights reserved.
+//
+
+import UIKit
+
+class StatesTableViewController: UITableViewController {
+
+    var anzahlStaaten: NSInteger = 0
+    var staaten: NSArray = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        navigationItem.title = "States \(anzahlStaaten)"
+    }
+
+    func initWithStates(states: NSArray, numberOfStates: NSInteger) -> Void {
+        anzahlStaaten = numberOfStates
+        staaten = states
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return anzahlStaaten
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell: UITableViewCell = UITableViewCell()
+
+        // Configure the cell...
+        cell.textLabel?.text = staaten[indexPath.row] as? String
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
